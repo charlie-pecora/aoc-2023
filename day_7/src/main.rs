a1aa2da92308e78123eef3b04c9e16f61c8049a3
charlie-pecora <charlie.pecora@gmail.com>
2023-12-07 18:16:56 -0800

    part 1

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -1,6 +1,6 @@
-use std::fs;
 use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::fs;
 
 fn main() {
     let filename = "input.txt";
@@ -17,9 +17,16 @@ fn main() {
             None => println!("Couldn't parse line {}", line),
         }
     }
+    hands.sort();
+    let mut score: u32 = 0;
+    for (i, hand) in hands.iter().enumerate() {
+        let ranking = u32::try_from(i).expect("error parsing enumerate") + 1;
+        score += ranking * hand.bid
+    }
+    println!("part 1 score: {score}");
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 enum Card {
     _2,
     _3,
@@ -47,6 +54,33 @@ enum HandType {
     FiveOfAKind,
 }
 
+impl HandType {
+    fn parse(card_counts: HashMap<Card, u32>) -> HandType {
+        let mut counts = card_counts.into_values().collect::<Vec<u32>>();
+        counts.sort();
+        counts.reverse();
+        if counts[0] == 5 {
+            return HandType::FiveOfAKind;
+        } else if counts[0] == 4 {
+            return HandType::FourOfAKind;
+        } else if counts[0] == 3 {
+            if counts[1] == 2 {
+                return HandType::FullHouse;
+            } else {
+                return HandType::ThreeOfAKind;
+            }
+        } else if counts[0] == 2 {
+            if counts[1] == 2 {
+                return HandType::TwoPair;
+            } else {
+                return HandType::OnePair;
+            }
+        } else {
+            return HandType::HighCard;
+        }
+    }
+}
+
 #[derive(Debug, Eq)]
 struct Hand {
     bid: u32,
@@ -67,6 +101,7 @@ impl Hand {
                 'A' => Card::A,
                 'K' => Card::K,
                 'Q' => Card::Q,
+                'J' => Card::J,
                 'T' => Card::T,
                 '9' => Card::_9,
                 '8' => Card::_8,
@@ -76,7 +111,7 @@ impl Hand {
                 '4' => Card::_4,
                 '3' => Card::_3,
                 '2' => Card::_2,
-                _ => panic!("no such card {c}")
+                _ => panic!("no such card {c}"),
             };
             cards.push(card);
             let count = card_counts.entry(card).or_insert(0);
@@ -85,7 +120,7 @@ impl Hand {
         return Some(Hand {
             bid: split[1].parse::<u32>().expect("invalid bid on hand {s}"),
             cards: cards,
-            hand_type: HandType::HighCard,
+            hand_type: HandType::parse(card_counts),
         });
     }
 }
